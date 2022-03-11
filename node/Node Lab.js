0c9789e31d46cff8974702bf46352674db343966
khongjason <97569744+khongjason@users.noreply.github.com>
2022-03-10 21:18:48 -0500

    Update Node Lab.js

--- node/Node Lab.js
+++ node/Node Lab.js
@@ -6,26 +6,33 @@ var ip = require('ip');
 http.createServer(function(req, res){
 
     if (req.url === "/") {
-        fs.readFile("./public/index.html", "UTF-8", function(err, body){
+        fs.readFile("./index.html", "UTF-8", function(err, body){
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(body);
     });
 }
     else if(req.url.match("/sysinfo")) {
         myHostName=os.hostname();
+	    //os oper
+ServerUpTime = new Date(os.uptime() * 1000).toISOString().substr(11, 8)
+totalMem = os.totalmem()/1048576;
+freeMem = os.freemem()/1048576;
+cpus = os.cpus().length+1;
+
+	    
         html=`    
         <!DOCTYPE html>
         <html>
           <head>
-            <title>Node JS Response</title>
+            <title>System Information</title>
           </head>
           <body>
             <p>Hostname: ${myHostName}</p>
             <p>IP: ${ip.address()}</p>
-            <p>Server Uptime: </p>
-            <p>Total Memory: </p>
-            <p>Free Memory: </p>
-            <p>Number of CPUs: </p>            
+            <p>ServerUptime: ${ServerUpTime} </p>
+            <p>Total Memory: ${totalMem} M </p>
+            <p>Free Memory: ${freeMem} M </p>
+            <p>Number of CPUs: ${cpus} </p>            
           </body>
         </html>` 
         res.writeHead(200, {"Content-Type": "text/html"});
